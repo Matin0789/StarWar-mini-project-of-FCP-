884639575bab929833a3a8458854e42fff456b4d
matin0789 <matin.a0789@gmail.com>
2023-12-25 00:56:01 +0330

    color and move added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <stdlib.h> 
 #include <ctime>
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
 #define row 10
 #define col 10
 
@@ -11,13 +30,16 @@ struct
 {
     int x = rand()%10, y= rand()%10;
     char c = '#';
-    int heal = 3;
+    int health = 3;
 }mySpaceShip;
 
 // function portotype
 void grandDraw(int condition[col][row]);
 void horizontalDraw(void);
 int howManyEnemy(void);
+void action(int condition[col][row]);
+void move(int condition[col][row]);
+void shoot(int condition[col][row]);
 
 // main function
 int main()
@@ -40,12 +62,14 @@ int main()
             }
         }
     }
-    while ( mySpaceShip.heal != 0 )
+    while ( mySpaceShip.health != 0 )
     { 
         grandDraw(condition);
+        action(condition);
         getchar();
     }
-        
+    system("cls");
+    cout << RED << "********************************GAME OVER********************************"<< endl << RESET;
     return 0;
 }
 
@@ -53,30 +77,31 @@ int main()
 // functions  
 void horizontalDraw(void)
 {
+    cout << BOLDBLUE;
     for (int i=0;i < col;i++)
         cout << " ---";
+    cout << RESET;
 }
-
 void grandDraw(int condition[col][row])
 {
     system("cls");
-    cout << "****************************************************STARWAR****************************************************";
-    cout << "\nhealth:" << mySpaceShip.heal << endl;
+    cout << BOLDGREEN <<"******************************************STARWAR******************************************" << RESET;
+    cout << "\nhealth:" << RED << mySpaceShip.health << RESET << endl;
     for (int i = 0;i < row;i++)
     {
         horizontalDraw();
         cout << endl;
         for (int j = 0;j < col;j++)
         {
-            cout << "| ";
+            cout << BOLDBLUE <<"| " << RESET;
             if (condition[j][i] == 1)
-                cout << "# ";
+                cout << BOLDGREEN <<"# " << RESET;
             else if (condition[j][i] == 2)
-                cout << "* ";
+                cout << BOLDRED << "* " << RESET;
             else
                 cout << "  ";
         }
-        cout << "|" << endl;
+        cout << BOLDBLUE<< "|" << RESET << endl;
     }
     horizontalDraw();
 }
@@ -85,4 +110,99 @@ int howManyEnemy(void)
 {
     int howMany = rand() % (row * (col - 1) - col) +  (col + 1);
     return howMany;
+}
+void action(int condition[col][row])
+{
+    char moveOrShoot;
+    cout << "\nmove or shoot[m/s]:";
+    cin >> moveOrShoot;
+    switch (moveOrShoot)
+    {
+    case 'm':
+        move(condition);
+        break;
+
+    case 's':
+        shoot(condition);
+        break;
+    default:
+        cout << RED << "undefined please enter to try again" << RESET << endl;
+        getchar();
+        break;
+    }
+}
+
+void move(int condition[col][row])
+{
+    char move;
+    cout << "up or down or left or right[u/d/l/r]:";
+    cin >> move;
+    switch (move)
+    {
+    case 'u':
+        if (condition[mySpaceShip.x][mySpaceShip.y - 1] == 0)
+        {
+            condition[mySpaceShip.x][mySpaceShip.y]=0;
+            mySpaceShip.y--;
+            condition[mySpaceShip.x][mySpaceShip.y]=1;
+        }
+        else
+        {
+            mySpaceShip.health--;
+            cout << RED << "!--ops--" << RESET;
+            getchar();
+        } 
+        break;
+    case 'd':
+        if (condition[mySpaceShip.x][mySpaceShip.y + 1] == 0)
+        {
+            condition[mySpaceShip.x][mySpaceShip.y]=0;
+            mySpaceShip.y++;
+            condition[mySpaceShip.x][mySpaceShip.y]=1;
+        }
+        else
+        {
+            mySpaceShip.health--;
+            cout << RED << "!--ops--" << RESET;
+            getchar();
+        }   
+        break;
+    case 'r':
+        if (condition[mySpaceShip.x + 1][mySpaceShip.y] == 0)
+        {
+            condition[mySpaceShip.x][mySpaceShip.y]=0;
+            mySpaceShip.x++;
+            condition[mySpaceShip.x][mySpaceShip.y]=1;
+        }
+        else
+        {
+            mySpaceShip.health--;
+            cout << RED << "!--ops--" << RESET;
+            getchar();
+        }
+        break;
+    case 'l':
+        if (condition[mySpaceShip.x - 1][mySpaceShip.y] == 0)
+        {
+            condition[mySpaceShip.x][mySpaceShip.y]=0;
+            mySpaceShip.x--;
+            condition[mySpaceShip.x][mySpaceShip.y]=1;
+        }
+        else
+        {
+            mySpaceShip.health--;
+            cout << RED << "!--ops--" << RESET;
+            getchar();
+        }
+        break;
+    default:
+        cout << RED << "undefined please enter to try again" << RESET << endl;
+        break;
+    }
+    
+}
+
+void shoot(int condition[col][row])
+{
+
 }
